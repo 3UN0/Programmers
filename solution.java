6acecd9e878caff96e74f6815a61cb989657f9ff
nyoung <seosilverzero@gmail.com>
2021-08-25 22:47:28 +0900

    DP_정수 삼각형

---
+++ solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int N = triangle.length;
+
+        for(int i=N-1; i>0;i--){
+            for(int j=0;j<i;j++){
+                triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+
+            }
+        }
+
+        answer = triangle[0][0];
+
+        return answer;
+    }
+}
