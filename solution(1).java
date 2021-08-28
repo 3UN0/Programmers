f358444b935da19b2295640eb5a9600f622f14a0
nyoung <seosilverzero@gmail.com>
2021-08-29 00:00:00 +0900

    DP_N으로 표현

---
+++ solution(1).java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+    
+    public void dfs(int count, int val) {
+        if(count>8) {
+            answer = -1;
+            return;
+        }
+        
+        
+    }
+}
