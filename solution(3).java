3777ff3d6ac07babb2350494951b2cc9bf59fe79
nyoung <seosilverzero@gmail.com>
2021-08-30 23:59:38 +0900

    Create solution(3).java

---
+++ solution(3).java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i=0;i<puddles.length;i++){
+            dp[puddles[i][0]][puddles[i][1]] = -1; 
+        }
+        
+        // 시작점
+        dp[1][1] = 1;
+    
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+               if(dp[i][j] == -1) {
+                   dp[i][j] = 0;
+                   continue;
+                } else {
+                   // 우측 이동
+                   dp[i][j] += dp[i-1][j] % 1000000007;
+                   // 하단 이동
+                   dp[i][j] += dp[i][j-1] % 1000000007;
+
+                } 
+                
+            }
+            
+        }
+        answer = dp[m][n] % 1000000007;
+        
+        
+        return answer;
+    }
+}
