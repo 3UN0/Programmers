4773240c1ca132f3c25583d2db5e6ee2f5db3c42
nyoung <seosilverzero@gmail.com>
2021-08-31 00:07:28 +0900

    DP_등굣길

--- solution(2).java
+++ solution(2).java
@@ -3,6 +3,7 @@ class Solution {
         int answer = 0;
         int[][] dp = new int[m+1][n+1];
         
+        // 웅덩이 -1
         for(int i=0;i<puddles.length;i++){
             dp[puddles[i][0]][puddles[i][1]] = -1; 
         }
@@ -13,21 +14,22 @@ class Solution {
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                if(dp[i][j] == -1) {
+                   // 계산값 영향 없도록
                    dp[i][j] = 0;
                    continue;
                 } else {
                    // 우측 이동
-                   dp[i][j] += dp[i-1][j] % 1000000007;
+                   if(i!=1)
+                       dp[i][j] += dp[i-1][j] % 1000000007;
                    // 하단 이동
-                   dp[i][j] += dp[i][j-1] % 1000000007;
+                   if(j!=1)
+                       dp[i][j] += dp[i][j-1] % 1000000007;
 
-                } 
-                
+                }
             }
-            
         }
+
         answer = dp[m][n] % 1000000007;
-        
         
         return answer;
     }
