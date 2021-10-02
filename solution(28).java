f80cd3e338c90060deb298b7ab3193fee774fa50
nyoung <seosilverzero@gmail.com>
2021-10-03 00:42:54 +0900

    섬머윈터코딩(2019)_멀쩡한 사각형

--- solution(28).java
+++ solution(28).java
@@ -2,12 +2,33 @@ class Solution {
     public long solution(int w, int h) {
         long answer = 1;
         
-        long rect = w * h;
-        if(w == h){
-            return rect - w;
-        } else {
-            answer = ((long)h -2) * (long)w/2 * 2;
-        }
+        // 가로 + 세로 - 최대공약수 
+        
+        // 형변환 : Long width = Long.valueOf(w);
+        
+        answer = (long)w*h - ((long)w + (long)h - gcd(w, h));    
+            
+            
         return answer;
+    }
+    
+    // 최대공약수
+    public long gcd(long width, long height){
+        long big = width;
+        long small = height;
+        // 큰, 작은 값 설정
+        if(width < height){
+            big = height;
+            small = width;
+        }
+        
+        long temp = 0;
+        // 작은 값으로 큰 값 나눠주기 
+        while(small > 0){
+            temp = big % small;
+            big = small; 
+            small = temp;
+        }
+        return big;
     }
 }
